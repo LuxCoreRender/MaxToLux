6700e2402985096408f546d83295c50b3d4b4c72
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2024-05-07 10:36:34 +0200

    Create LuxLight.cpp

---
+++ MaxToLux/LuxLight.cpp
@@ -0,0 +1,20 @@
+//**************************************************************************/
+// Copyright (c) 2015-2024 Luxrender.
+// All rights reserved.
+// 
+//**************************************************************************/
+// DESCRIPTION: Contains the Dll Entry stuff
+// AUTHOR: Omid Ghotbi (TAO) omid.ghotbi@gmail.com www.3dfine.com
+//***************************************************************************/
+
+
+#include <algorithm>
+#include <string>
+#include "max.h"
+#include "LuxLights.h"
+#include <maxscript\maxscript.h>
+#include "LuxMaterials.h"
+#include "LuxMaps.h"
+#include "LuxUtils.h"
+#include "Classes.h"
+#include <luxcore.h>
