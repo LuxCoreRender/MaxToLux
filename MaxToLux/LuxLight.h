9ee195fc68a706ece1622717a78663cc23fd078d
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2024-05-07 10:36:13 +0200

    Update LuxLight.h

--- MaxToLux/LuxLight.h
+++ MaxToLux/LuxLight.h
@@ -12,3 +12,26 @@
 #include <algorithm>
 #include <luxcore.h>
 #include <iparamb2.h>
+
+class MaxToLuxLights
+{
+public:
+	//MaxToLuxLights();
+	//~MaxToLuxLights();
+
+	std::string exportOmni(INode* Light);
+	std::string exportSkyLight(INode* Light);
+	std::string exportDiright(INode* Light);
+	std::string exportSpotLight(INode* Light);
+	std::string exportLuxPointLight(INode* Light);
+	std::string exportLuxSphereLight(INode * Light);
+	std::string exportLuxLaserLight(INode* Light);
+	std::string exportLuxSpotLight(INode * Light);
+	std::string exportLuxSkyLight(INode * Light);
+	std::string exportLuxSunLight(INode * Light);
+	void setDefaultSkyLight(luxcore::Scene &scene);
+	void setInfinitLightPreview(luxcore::Scene &scene);
+	bool setInfinitLight(luxcore::Scene & scene, Texmap * envMap);
+	void MaxToLuxLights::setLight(INode * currNode, luxcore::Scene &scene, TimeValue t);
+	int MaxToLuxLights::isSupportedLight(Object * obj);
+};
