7b1db56f958620f532a2022ff8068b3d334441d0
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2024-05-07 10:32:13 +0200

    Create LuxDlg.h

---
+++ MaxToLux/LuxDlg.h
@@ -0,0 +1,20 @@
+//**************************************************************************/
+// Copyright (c) 2015-2024 Luxrender.
+// All rights reserved.
+// 
+//**************************************************************************/
+// DESCRIPTION: Contains the Dll Entry stuff
+// AUTHOR: Omid Ghotbi (TAO) omid.ghotbi@gmail.com www.3dfine.com
+//***************************************************************************/
+
+#pragma once
+
+#include "main.h"
+#include "MaxToLux.h"
+#include <maxscript\maxscript.h>
+#include "3dsmaxport.h"
+#include <string>
+#include "Classes.h""
+
+#undef min
+#undef max
