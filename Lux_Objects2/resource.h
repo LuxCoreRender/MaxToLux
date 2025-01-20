2c854e1e7b0d5e97b32745060707b02296748656
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2025-01-20 21:11:36 +0100

    Create resource.h

---
+++ Lux_Objects2/resource.h
@@ -0,0 +1,103 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LuxObjects.rc
+//
+#define IDS_LIBDESCRIPTION              1
+#define IDS_CATEGORY                    2
+#define IDS_CLASS_NAME                  3
+#define IDS_LIGHT_POINT_CLASS_NAME      3
+#define IDS_PARAMS                      4
+#define IDS_LIGHT_POINT_PARAMS          4
+#define IDS_SPIN                        5
+#define IDS_LIGHT_COLOR                 5
+#define IDS_LIGHT_COMMON_PARAMS         6
+#define IDS_LIGHT_POWER_SPIN            7
+#define IDS_LIGHT_GAIN_SPIN             8
+#define IDS_LIGHT_SAMPLE_SPIN           9
+#define IDS_LIGHT_IMPORTANCE_SPIN       10
+#define IDS_LIGHT_SPHERE_CLASS_NAME     11
+#define IDS_LIGHT_SPOT_CLASS_NAME       12
+#define IDS_TARGET_CLASS                13
+#define IDS_TARGET                      14
+#define IDS_PRIMITIVES                  15
+#define IDS_DOT_TARGET                  16
+#define IDS_LIGHT_CREATE                17
+#define IDS_TARGET_SPOT_CLASS           18
+#define IDS_LIGHT_SKY_CLASS_NAME        19
+#define IDS_LIGHT_SUN_CLASS_NAME        20
+#define IDS_TARGET_SUN_CLASS            21
+#define IDS_TARGET_SKY_CLASS            22
+#define IDS_TARGET_LASER_CLASS          23
+#define IDS_LIGHT_LASER_CLASS_NAME      24
+#define IDS_LIGHT_MULTIPLIER_SPIN       25
+#define IDD_PANEL                       101
+#define IDD_LIGHT_COMMON_PANEL          101
+#define IDD_LIGHT_POINT                 102
+#define IDD_LIGHT_SPHERE                103
+#define IDD_LIGHT_SPOT                  104
+#define IDD_LIGHT_SKY                   105
+#define IDD_LIGHT_SUN                   106
+#define IDD_LIGHT_LASER                 107
+#define IDC_CLOSEBUTTON                 1000
+#define IDC_DOSTUFF                     1000
+#define IDC_LIGHT_POINT_COLOR           1002
+#define IDC_LIGHT_POINT_POWER           1003
+#define IDC_LIGHT_POINT_POWER_SPIN      1004
+#define IDC_LIGHT_POINT_EFFICENCY       1005
+#define IDC_LIGHT_POINT_EFFICENCY_SPIN  1006
+#define IDC_LIGHT_POINT_REDIUS          1007
+#define IDC_LIGHT_POINT_REDIUS_SPIN     1008
+#define IDC_LIGHT_SPOT_CONE_DELTA       1009
+#define IDC_LIGHT_SPOT_CONE_DELTA_SPIN  1010
+#define IDC_LIGHT_SPOT_POWER            1011
+#define IDC_LIGHT_SPOT_POWER_SPIN       1012
+#define IDC_LIGHT_SPOT_COLOR            1013
+#define IDC_LIGHT_SPOT_EFFICENCY        1014
+#define IDC_LIGHT_SPOT_EFFICENCY_SPIN   1015
+#define IDC_LIGHT_SPOT_CONE             1016
+#define IDC_LIGHT_SPOT_CONE_SPIN        1017
+#define IDC_LIGHT_SKY_TURBIDITY         1019
+#define IDC_LIGHT_SKY_TURBIDITY_SPIN    1020
+#define IDC_LIGHT_SKY_ALBEDO_COLOR      1027
+#define IDC_LIGHT_SUN_RELSIZE           1028
+#define IDC_LIGHT_SKY_GROUND_COLOR      1029
+#define IDC_LIGHT_SUN_RELSIZE_SPIN      1029
+#define IDC_LIGHT_SKY_DIFFUSE_CHECK     1036
+#define IDC_LIGHT_SKY_GLOSSY_CHECK      1037
+#define IDC_LIGHT_SKY_SPECULAR_CHECK    1038
+#define IDC_LIGHT_SKY_ENABLE_GROUND_CHECK 1039
+#define IDC_LIGHT_SUN_TURBIDITY         1039
+#define IDC_LIGHT_SUN_TURBIDITY_SPIN    1040
+#define IDC_LIGHT_SUN_DIFFUSE_CHECK     1041
+#define IDC_LIGHT_SUN_GLOSSY_CHECK      1042
+#define IDC_LIGHT_SUN_SPECULAR_CHECK    1043
+#define IDC_LIGHT_LASER_POWER           1044
+#define IDC_LIGHT_LASER_POWER_SPIN      1045
+#define IDC_LIGHT_LASER_EFFICENCY       1046
+#define IDC_LIGHT_LASER_EFFICENCY_SPIN  1047
+#define IDC_LIGHT_LASER_RADIUS          1048
+#define IDC_LIGHT_LASER_RADIUS_SPIN     1049
+#define IDC_LIGHT_LASER_COLOR           1051
+#define IDC_LIGHT_MULTIPLAIER           1052
+#define IDC_COLOR                       1456
+#define IDC_EDIT                        1490
+#define IDC_LIGHT_SAMPLE                1490
+#define IDC_LIGHT_ID                    1491
+#define IDC_LIGHT_IMPORTANCE            1492
+#define IDC_LIGHT_GAIN                  1493
+#define IDC_SPIN                        1496
+#define IDC_LIGHT_SAMPLE_SPIN           1496
+#define IDC_LIGHT_ID_SPIN               1497
+#define IDC_LIGHT_IMPORTANCE_SPIN       1498
+#define IDC_LIGHT_MULTIPLAIER_SPIN      1499
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1053
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
