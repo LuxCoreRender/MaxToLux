fa01d592018588e64ddcb717aaf2dbddb28ef59f
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2025-01-20 21:11:41 +0100

    Create resourceOverride.h

---
+++ Lux_Objects2/resourceOverride.h
@@ -0,0 +1,13 @@
+//
+//	resourceOverride.h
+//
+//
+//	This header file contains product specific resource override code.
+//	It remaps max resource ID to derivative product alternatives.
+//
+//
+
+#ifndef _RESOURCEOVERRIDE_H
+#define _RESOURCEOVERRIDE_H
+
+#endif	// _RESOURCEOVERRIDE_H
